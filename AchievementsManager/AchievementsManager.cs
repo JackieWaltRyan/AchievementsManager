4a70dc45dd5e40778471f7f4fc37386b0a272929
Jackie Ryan <jackiewaltryan@gmail.com>
2025-07-02 18:18:16 +0300

    AchievementsManager

---
+++ AchievementsManager/AchievementsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using ArchiSteamFarm.Core;
+using ArchiSteamFarm.Plugins.Interfaces;
+
+namespace AchievementsManager;
+
+internal sealed class AchievementsManager : IGitHubPluginUpdates {
+    public string Name => nameof(AchievementsManager);
+    public string RepositoryName => "JackieWaltRyan/AchievementsManager";
+    public Version Version => typeof(AchievementsManager).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
+
+    public Task OnLoaded() {
+        ASF.ArchiLogger.LogGenericInfo($"Hello {Name}!");
+
+        return Task.CompletedTask;
+    }
+}
